f41501b70e857931dfacaa374e6794c00f4b0f1e
DeVdistress <devdistress@gmail.com>
2022-12-21 18:18:57 +0300

    + added CLion project settings

--- src/test.cpp
+++ src/test.cpp
@@ -1,11 +1,16 @@
 #include<iostream>
-#include<string_view>
+#include <list>
 
 int main(){
 
         auto i = 1;
 
-        std::cout<< std::boolalpha << i <<"\n";
+        std::cout << std::boolalpha << i <<"\n";
+
+        auto ls = std::list<int>({1, 2, 3, 4, 5});
+        for (const auto i : ls) {
+            std::cout << i << std::endl;
+        }
 
         return 0;
 }
