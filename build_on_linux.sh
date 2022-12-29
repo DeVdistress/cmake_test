647b916bd42e98f2a25f2919358a3fdf8aca4747
DeVdistress <devdistress@gmail.com>
2022-12-29 18:12:39 +0300

    + added ninja

--- build_on_linux.sh
+++ build_on_linux.sh
@@ -2,13 +2,25 @@
 
 rm -r ./build
 
-if [[ $1 = "d" ]]; then
-   echo "build debug..."
+if [ "$1" = "j" ] && [ "$2" = "d"]; then
+   echo "build Ninja debug..."
+   cmake -DCMAKE_BUILD_TYPE=Release -G "Ninja" -B build
+elif [[ $1 = "j" ]]; then
+   echo "build Ninja release"
+   cmake -DCMAKE_BUILD_TYPE=Release -G "Ninja" -B build
+elif [[ $1 = "d" ]]; then
+   echo "build Make debug..."
    cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -B build
 else
-   echo "build release..."
+   echo "build Make release"
    cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -B build
 fi
 
 cd ./build
-make -j$(nproc --all)
+
+if [[ $1 = "j" ]]; then
+   ninja -j$(nproc --all)
+else
+   make -j$(nproc --all)
+fi
+
